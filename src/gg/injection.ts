79da551e02f1a4ae88fe71454ac612a7c00e45fe
Marcel Siewert <bitkomponist@gmail.com>
2024-07-28 14:08:24 +0200

    injection docs

--- src/gg/injection.ts
+++ src/gg/injection.ts
@@ -5,6 +5,11 @@ const typeNameRegistry = new Map<string, InjectableType>();
 
 const typeRegistry = new Map<InjectableType, string>();
 
+/**
+ * Class decorator to register a given target class as injectable
+ *
+ * @returns Injectable class definition
+ */
 export function Injectable(): (target: InjectableType) => void {
   return (target) => {
     if (typeNameRegistry.has(target.name)) {
@@ -15,6 +20,12 @@ export function Injectable(): (target: InjectableType) => void {
   };
 }
 
+/**
+ * Resolve a class definition by a given type name
+ *
+ * @param typeName - String representation of a classes name
+ * @returns Class definition
+ */
 export function getInjectableType<T extends object = object>(typeName: string) {
   return typeNameRegistry.get(typeName) as InjectableType<T> | undefined;
 }
