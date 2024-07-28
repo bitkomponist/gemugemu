08b82c0506a807b88fe623f5d47c0d0fbcbd7181
Marcel Siewert <bitkomponist@gmail.com>
2024-07-28 14:12:15 +0200

    prefab docs

--- src/gg/prefab.ts
+++ src/gg/prefab.ts
@@ -1,8 +1,25 @@
 import { merge } from 'lodash';
 import { Entity, EntityDescriptor } from './entity';
 
+/**
+ * Abstract class to compose building blocks, partial bits of hierarchy (e.g. partial entity trees),
+ * with optional custom properties
+ */
 export abstract class Prefab<P extends object = object> {
+  /**
+   * Build a custom partial entity hierarchy / tree to reuse inside of your application
+   *
+   * @param props - Custom properties to modify the resulting entity descriptor with
+   */
   protected abstract build(props?: P): EntityDescriptor;
+
+  /**
+   * Create a descriptor of this prefab, with optional overrides
+   *
+   * @param propsWithOverrides - Custom properties to modify the resulting entity descriptor with,
+   *   merge with custom overrides to further modify
+   * @returns Descriptor result
+   */
   describe(propsWithOverrides: Partial<P> & { overrides?: Partial<EntityDescriptor> } = {}) {
     const { overrides, ...props } = propsWithOverrides;
     const descriptor = this.build(props as P);
